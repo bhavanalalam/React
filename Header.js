55737f975168bfd5e46934cc785bf8fcb65a3af0
bhavanalalam <bhavanalalam4@gmail.com>
2025-10-20 22:04:32 +0530

    Add files via upload

--- Header.js
+++ Header.js
@@ -1,11 +1,31 @@
-const Header =() =>{
-    return(
-        <>
-        <div className="header">
-           <div>
-       <h1>Header Component</h1>
-            </div> 
+import AppIcon from "./AppIcon";
+import TitleComponent from "./TitleComponent";
+import TitleTagLine from "./TitleTagLine";
+const Header = () => {
+  return (
+    <div className="header" style={{ 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      padding: "20px" ,
+      backgroundColor: "#f8f8f8",
+        borderBottom: "1px solid #ddd",
+    }}>
+      <div className="brand"
+      style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "20px",
+        }}>
+        <div>
+          <AppIcon />
         </div>
-        </>
-    )
-}
+        <div>
+          <TitleComponent />
+          <TitleTagLine />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Header;
