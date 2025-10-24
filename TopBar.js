e2050898721cc5fd567acad9a6811d20e8368810
bhavanalalam <bhavanalalam4@gmail.com>
2025-10-24 13:11:50 +0530

    Update TopBar.js

--- TopBar.js
+++ TopBar.js
@@ -1,17 +1,33 @@
-import React from "react";
-const TopBar = () =>{
-    return(
-        <section className="topBarSection">
-            <div className="companyTitle">
-                <h3>Yummate</h3>
-            </div>
-            <div className="searchBar">
-                <input type="text" placeholder="search.."/>
-            </div>
-            <div className="userAuth">
-                Login/Signup
-            </div>
-        </section>
-    )
-}
+import react from "react";
+import { useState, useEffect } from "react";
+const TopBar = ({ allRestaurants, setFilteredRestaurants }) => {
+  const [searchText, setSearchText] = useState("");
+  const search = () => {
+    const filtered = allRestaurants.filter((res) =>
+      res.info.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredRestaurants(filtered);
+
+  };
+
+  return (
+    <section className="topBarSection" style={{ padding: "20px" }}>
+      <div className="companyTitle">
+        <h3>Yummate</h3>
+      </div>
+      <div>
+        <input
+          type="text" placeholder="Search" value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </div>
+
+      <div className="userAuth" style={{ marginTop: "10px" }}>
+        Login/Signup
+      </div>
+    </section>
+  );
+};
+
 export default TopBar;
